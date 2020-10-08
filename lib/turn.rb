b2536c0da1f4df99606928bb03f313e3ccbbec46
Cameron Bisca <cjbisca@gmail.com>
2020-10-08 20:36:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,9 +8,32 @@
  # ask for input again until you get a valid input
 #end
 
+def current_player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
+end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    player_move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
+  }
+  counter
+end
   
-
 
 def input_to_index(index)
   index = index.to_i - 1
