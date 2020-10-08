005a320be1f393ec8b0525a78aa2dce8f583e80e
Cameron Bisca <cjbisca@gmail.com>
2020-10-08 20:27:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,19 +19,7 @@ def turn(board)
    display_board(board)
 end
   
-def turn_count(board)
-   counter = 0
-   board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
-      end
-   end
-   counter
-end
 
-def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
-end
 
 def input_to_index(index)
   index = index.to_i - 1
