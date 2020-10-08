08694432e8472cd39068eaf08d8904ba577934ab
Cameron Bisca <cjbisca@gmail.com>
2020-10-08 21:26:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,8 +14,10 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, value = "X")
-      turn(board)
+  else 
+    turn(board)
   end
+  display_board(board)
 end
 
 def move(array, index, value = "X")
