4cced125984a9c4511bc19e23a77501fc803bd72
Cameron Bisca <cjbisca@gmail.com>
2020-10-08 21:16:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,15 @@ def valid_move?(board, index)
   end
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    return false 
+  end
 
+  if board[index] == "X" ||  board[index] == "O"
+    return true 
+  end
+end
 
 def input_to_index(user_input)
   user_input = user_input.to_i - 1
