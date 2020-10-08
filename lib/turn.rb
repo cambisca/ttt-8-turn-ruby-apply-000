deb5701e7eae9942cdcaf86565b3a38fc4fa4f04
Cameron Bisca <cjbisca@gmail.com>
2020-10-08 21:13:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,49 +8,5 @@
  # ask for input again until you get a valid input
 #end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-    if valid_move?(board, index)
-      move(board, index, value = "X")
-        turn(board)
-    end
-  display_board(board)
-end
 
 
-def input_to_index(index)
-  index = index.to_i - 1
-end
-
-def move(array, index, value = "X")
-  array[index] = value
-end
-
-def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0, 8) 
-    return true 
-
-  else 
-    return false  
-  end
-end
-
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil 
-    return false 
-  end
-  if board[index] == "X" ||  board[index] == "O"
-    return true 
-  end
-end
-
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end 
