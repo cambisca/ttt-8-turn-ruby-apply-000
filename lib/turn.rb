a6a3403769ddf4324166f19446157f6f55476d4d
Cameron Bisca <cjbisca@gmail.com>
2020-10-08 20:36:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,16 +8,7 @@
  # ask for input again until you get a valid input
 #end
 
-def turn(board)
-   puts "Please enter 1-9:"
-   user_input = gets.strip
-   index = input_to_index(user_input)
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
-        turn(board)
-   end
-   display_board(board)
-end
+
   
 
 
